db5ee35d70d8eb0a7e3d92d91e5da4ccdf272d02
Aman Joshi <amanjoshi668@gamil.com>
2020-05-14 14:38:12 +0530

    Initial Commit

---
+++ code/cnn_torch_model.py
@@ -0,0 +1,130 @@
+import os
+import cPickle
+import random
+import numpy as np
+import scipy.parse as sp
+from w2v import load_word2vec
+
+import torch
+from torch import nn
+
+class CNN_model(nn.Module):
+    def __init__(self, args):
+        # Model Hyperparameters
+        self.sequence_length = args.sequence_length
+        self.embedding_dim = args.embedding_dim
+        self.filter_sizes = args.filter_sizes
+        self.num_filters = args.num_filters
+        self.pooling_units = args.pooling_units
+        self.pooling_type = args.pooling_type
+        self.hidden_dims = args.hidden_dims
+
+        # Training Hyperparameters
+        self.batch_size = args.batch_size
+        self.num_epochs = args.num_epochs
+
+        # Model variation and w2v pretrain type
+        self.model_variation = args.model_variation        # CNN-rand | CNN-pretrain
+        self.pretrain_type = args.pretrain_type
+
+        # Fix random seed
+        np.random.seed(1126)
+        random.seed(1126)
+        # self.rng = torch.shared_randomstreams.RandomStreams(seed=1126)
+
+        # Model
+        self.embedding = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embedding_dim)
+        if self.model_variation == "pretrain":
+            self.embedding.load_state_dict({'weight': self.embedding_weights})
+        elif self.model_variation != 'random':
+            raise NotImplementedError('Unknown model_variation')
+        self.dropout1 = nn.Dropout(p = 0.25)
+        self.convs = []
+        for fsz in self.filter_sizes:
+            l_conv = nn.Conv1d(in_channels = self.embedding_dim, out_channels=self.num_filters, kernel_size=fsz, stride=2)
+            # pool_size = l_conv_shape[-1] // self.pooling_units
+            if self.pooling_type == 'average':
+                l_pool = nn.AdaptiveAvgPool1d(self.dynamic_pool_length)
+            elif self.pooling_type == 'max':
+                l_pool = nn.AdaptiveMaxPool1d(self.dynamic_pool_length)
+            else:
+                raise NotImplementedError('Unknown pooling layer')
+            self.convs.append((l_conv, l_pool))
+        self.__in_shape = self.filter_sizes * self.num_filters * self.dynamic_pool_length
+        self.hidden = nn.Linear(in_features=self.__in_shape, out_features=self.hidden_dims)
+        self.dropout2 = nn.Dropout(p = 0.5)
+        self.out_layer = nn.Linear(in_features=self.hidden_dims, out_features=self.output_dim)
+
+
+    def add_data(self, X_trn, Y_trn):
+        """
+            add data to the model.
+        """
+        self.X_trn = X_trn
+        self.Y_trn = Y_trn
+        self.output_dim = Y_trn.shape[1]
+
+    def add_pretrain(self, vocabulary, vocabulary_inv):
+        print ('model_variaton:', self.model_variation)
+        if self.model_variation=='pretrain':
+            embedding_weights = load_word2vec(self.pretrain_type, vocabulary_inv, self.embedding_dim)
+        elif self.model_variation=='random':
+            embedding_weights = None
+        else:
+            raise ValueError('Unknown model variation')
+        self.embedding_weights = embedding_weights
+        self.vocab_size = len(vocabulary)
+
+    def forward(self, X):
+        X = self.embedding(X)
+        X = X.unsqueeze(1)
+        X = self.dropout1(X)
+        X = [pool(torch.relu(conv(X))) for (conv, pool) in self.convs]
+        X = torch.cat(X, 1)
+        X = toch.relu(self.hidden(X.view(-1, self.__in_shape)))
+        X = self.dropout2(X)
+        X = torch.sigmoid(self.output_dim(X))
+        return X
+
+    def fit(self, lr=0.01):
+        nr_trn_num = self.X_trn.shape[0]
+        nr_batches = int(np.ceil(nr_trn_num / float(self.batch_size)))
+        #nr_batches = nr_trn_num // self.batch_size
+        self.train()
+        criterion = nn.BCELoss
+        optimizer = torch.optim.Adam(self.parameters, lr = lr)
+        trn_loss = []
+        for batch_idx in np.random.permutation(range(nr_batches)):
+            start_idx = batch_idx * self.batch_size
+            end_idx = min((batch_idx + 1) * self.batch_size, nr_trn_num)
+            #end_idx = (batch_idx + 1) * self.batch_size
+            X = self.X_trn[start_idx:end_idx]
+            Y = self.Y_trn[start_idx:end_idx].toarray()
+            Y_pred = self.forward(X)
+            loss = criterion(Y_pred, Y)
+            optimizer.zero_grad()
+            loss.backward() # Backpropagation
+            optimizer.step() 
+            trn_loss.append(loss.item)
+        return np.mean(loss)
+    
+    def predict(self, X_tst=None, batch_size = 8192, top_k=50, max_k=None):
+        self.eval()
+        nr_tst_num = X_tst.shape[0]
+        nr_batches = int(np.ceil(nr_tst_num / float(batch_size)))
+        row_idx_list, col_idx_list, val_idx_list = [], [], []
+        #X_pred = np.zeros((X_tst.shape[0], self.hidden_dims))
+        for batch_idx in range(nr_batches):
+            start_idx = batch_idx * batch_size
+            end_idx = min((batch_idx + 1) * batch_size, nr_tst_num)
+            Y_pred = self.forward(X_tst[start_idx:end_idx])
+            #X_pred[start_idx:end_idx, :] = X_hidden
+            for i in range(Y_pred.shape[0]):
+                sorted_idx = np.argpartition(-Y_pred[i, :], top_k)[:top_k]
+                row_idx_list += [i + start_idx] * top_k
+                col_idx_list += (sorted_idx).tolist()
+                val_idx_list += Y_pred[i, sorted_idx].tolist()
+        m = max(row_idx_list) + 1
+        n = max_k
+        Y_pred = sp.csr_matrix((val_idx_list, (row_idx_list, col_idx_list)), shape=(m, n))
+        return Y_pred
